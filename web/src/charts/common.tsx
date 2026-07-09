45ad2d2af2014966ef39254baa7f3a0b4e1ddb1a
Jesus Santos <jesus.santos@heaven-labs.com>
2026-07-09 19:31:44 -0300

    fix: add minHeight 100 to chart wrapper to prevent ECharts crash on 0-height container

--- web/src/charts/common.tsx
+++ web/src/charts/common.tsx
@@ -696,7 +696,7 @@ export const EChartsContainer = memo(function EChartsContainer({ option, height:
   }, [])
 
   return (
-    <div ref={wrapperRef} style={{ position: 'relative', height: '100%' }}>
+    <div ref={wrapperRef} style={{ position: 'relative', height: '100%', minHeight: 100 }}>
       <div data-testid="chart-container" ref={containerRef} style={{ position: 'absolute', inset: 0 }} />
       {showReset && (
         <button
